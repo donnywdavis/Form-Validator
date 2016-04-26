44d9cd96f35538075497c390770760c2a7be0eda
Donny Davis <donnywdavis@icloud.com>
2016-04-26 15:59:18 -0400

    When enter is pressed validate each text field before moving to the next

--- Form-Validator/Form-Validator/ViewController.m
+++ Form-Validator/Form-Validator/ViewController.m
@@ -18,6 +18,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *stateTextField;
 @property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
 
+@property (strong, nonatomic) FormValidator *formValidator;
+
 @end
 
 @implementation ViewController
@@ -25,9 +27,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
     // Let's set our title for the view
     self.title = @"Validator";
+    
+    // Initialize our form validator before we forget again
+    self.formValidator = [[FormValidator alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,6 +39,69 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if ([textField isEqual:self.nameTextField]) {
+        if ([self.formValidator isValidName:textField.text]) {
+            [self.nameTextField resignFirstResponder];
+            [self.addressTextField becomeFirstResponder];
+            self.nameTextField.backgroundColor = [UIColor clearColor];
+            return YES;
+        } else {
+            self.nameTextField.backgroundColor = [UIColor redColor];
+        }
+        
+    } else if ([textField isEqual:self.addressTextField]) {
+        if ([self.formValidator isValidAddress:textField.text]) {
+            [self.addressTextField resignFirstResponder];
+            [self.cityTextField becomeFirstResponder];
+            self.addressTextField.backgroundColor = [UIColor clearColor];
+            return YES;
+        } else {
+            self.addressTextField.backgroundColor = [UIColor redColor];
+        }
+        
+    } else if ([textField isEqual:self.cityTextField]) {
+        if ([self.formValidator isValidCity:textField.text]) {
+            [self.cityTextField resignFirstResponder];
+            [self.stateTextField becomeFirstResponder];
+            self.cityTextField.backgroundColor = [UIColor clearColor];
+            return YES;
+        } else {
+            self.cityTextField.backgroundColor = [UIColor redColor];
+        }
+        
+    } else if ([textField isEqual:self.stateTextField]) {
+        if ([self.formValidator isValidState:textField.text]) {
+            [self.stateTextField resignFirstResponder];
+            [self.zipTextField becomeFirstResponder];
+            self.stateTextField.backgroundColor = [UIColor clearColor];
+            return YES;
+        } else {
+            self.stateTextField.backgroundColor = [UIColor redColor];
+        }
+        
+    } else if ([textField isEqual:self.zipTextField]) {
+        if ([self.formValidator isValidZipCode:textField.text]) {
+            [self.zipTextField resignFirstResponder];
+            [self.phoneTextField becomeFirstResponder];
+            self.zipTextField.backgroundColor = [UIColor clearColor];
+            return YES;
+        } else {
+            self.zipTextField.backgroundColor = [UIColor redColor];
+        }
+        
+    } else if ([textField isEqual:self.phoneTextField]) {
+        if ([self.formValidator isValidPhoneNumber:textField.text]) {
+            [self.phoneTextField resignFirstResponder];
+            self.phoneTextField.backgroundColor = [UIColor clearColor];
+            return YES;
+        } else {
+            self.phoneTextField.backgroundColor = [UIColor redColor];
+        }
+        
+    }
+    
+    return NO;
+}
 
 @end
